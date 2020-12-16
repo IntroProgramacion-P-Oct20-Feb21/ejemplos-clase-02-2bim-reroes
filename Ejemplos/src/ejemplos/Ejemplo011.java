6a4692c07d66edd5f3856d6cc7991b759072a2fb
René Rolando Elizalde Solano <reroes799@gmail.com>
2020-12-16 08:54:30 -0500

    Initial commit

---
+++ Ejemplos/src/ejemplos/Ejemplo011.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar y generar arreglos en Java
+        
+        // Se declara un arreglo de tipo double, llamado promedios
+        double[] promediosMateria1 = {8.1, 9, 9.5, 10, 9.9};
+        double[] promediosMateria2 = {7.9, 9.2, 9.7, 9.8, 9.7};
+        String[] estudiantes = {"Luis", "Ana", "Mónica", "José", "Juan"};
+        double[] promediosFinales = new double[5];
+        
+        double sumaNotas;
+        double promedio;
+        
+        String mensajeFinal = ""; // variable que acumula un reporte final
+        // Se hace uso del ciclo repetitivo for para recorrer los arreglos
+        // y presentar los valore de las posiciones
+        
+        for (int i = 0; i < promediosMateria1.length; i++) {
+            // se suma el valor de la posición i del arreglo 
+            // promediosMateria1 y promediosMateria2
+            sumaNotas = promediosMateria1[i] + promediosMateria2[i];
+            // se obtiene el promedio de la suma de las notas
+            promedio = sumaNotas / 2;
+            // se asigna a la posición i del arreglo promediosFinales
+            promediosFinales[i] = promedio;
+        }
+        
+        for (int i = 0; i < promediosMateria1.length; i++) { 
+            
+            mensajeFinal = String.format("%sEstudiante:%s - promedio final: "
+                    + "%.4f\n", 
+                    mensajeFinal,
+                    estudiantes[i],
+                    promediosFinales[i]);
+        }
+        
+        System.out.printf("%s\n", mensajeFinal);
+        
+    }
+
+}
